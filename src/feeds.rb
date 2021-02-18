d9135756949aa57db6180fad6ff0b03df4bf188c
Michael Camilleri <mike@inqk.net>
2021-02-18 09:22:54 +0900

    Support excluding feeds at command line

--- src/feeds.rb
+++ src/feeds.rb
@@ -9,11 +9,13 @@ def save(name, contents)
   File.write(path + name, contents)
 end
 
-file_names = if ARGV.empty?
-               Dir.each_child("input")
-             else
-               ARGV.each
-             end
+def file_names
+  return Dir.each_child("input") if ARGV.empty?
+  return ARGV.each unless ARGV[0] == "-e"
+
+  excluded = ARGV.slice(1..-1)
+  Dir.each_child("input").reject { |fn| excluded.include? fn }
+end
 
 file_names.each do |file_name|
   require_relative "input/#{file_name}"
