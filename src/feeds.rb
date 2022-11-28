8bec2d3b7eb834013c32149f70d52c5db2a55e59
Michael Camilleri <mike@inqk.net>
2022-11-29 04:22:30 +0900

    Rename directory input to rules, add more sophisticated option parsing

--- src/feeds.rb
+++ src/feeds.rb
@@ -1,28 +1,77 @@
 # frozen_string_literal: true
 
+require "optparse"
 require "timeliness"
 
 Timeliness.add_formats(:date, "mmm d, yy")
 
-def save(name, contents)
-  path = "../public/"
-  File.write(path + name, contents)
+def options
+  return @options unless @options.nil?
+
+  @options = {
+    includes: [],
+    excludes: [],
+    rules_dir: "rules",
+    output_dir: "outputs"
+  }
+
+  OptionParser.new do |parser|
+    parser.banner = <<~MSG
+      Usage: feeds.rb [options]
+
+      Fetches and parses feeds specified in rules files and writes the outputs
+      to the outputs directory.
+
+      Feeds are parsed in accordance with specified rules files. If no rules
+      files are specified, the script will use all files in the rules
+      directory.
+    MSG
+    parser.separator ""
+    parser.separator "Specific options:"
+
+    parser.on("-i", "--include FILE", "Include rules in FILE.") do |name|
+      @options[:includes].push name
+    end
+
+    parser.on("-e", "--exclude FILE", "Exclude rules in FILE.") do |name|
+      @options[:excludes].push name
+    end
+
+    parser.on("-o", "--output-dir DIR", "Use DIR to save outputs. Default: 'outputs'") do |dir|
+      @options[:output_dir] = dir
+    end
+
+    parser.on("-r", "--rules-dir DIR", "Use DIR as source for rules. Default: 'rules'") do |dir|
+      @options[:rules_dir] = dir
+    end
+
+    parser.on_tail("-h", "--help", "Show this message") do
+      puts parser
+      exit
+    end
+  end.parse!
+
+  @options
 end
 
-def file_names
-  all = Dir.each_child("input").map { |fn| fn.prepend "input/" }
+def save(name, contents)
+  File.write(name, contents)
+end
 
-  if ARGV.empty?
+def file_names(options)
+  return options[:includes] unless options[:includes].empty?
+
+  rules_dir = options[:rules_dir]
+  all = Dir.each_child(rules_dir).map { |name| [rules_dir, name].join("/") }
+
+  if options[:excludes].empty?
     all
-  elsif ARGV[0] != "-e"
-    ARGV.each
   else
-    excluded = ARGV.slice(1..-1)
-    all.reject { |fn| excluded.include? fn }
+    all.reject { |name| options[:excludes].include? name }
   end
 end
 
-file_names.each do |file_name|
+file_names(options).each do |file_name|
   require_relative file_name
   base_name = file_name.split("/").last.delete_suffix(".rb")
   class_name = base_name
@@ -31,9 +80,9 @@ file_names.each do |file_name|
     .join
   klass = Object.const_get class_name
   begin
-    save "#{base_name}.xml", klass.new.feed
+    save "#{options[:output_dir]}/#{base_name}.xml", klass.new.feed
   rescue => e
-    puts "#{base_name} failed"
+    puts "Failed to parse rules in #{file_name}"
     puts e.message
   end
 end
