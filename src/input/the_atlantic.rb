f8735244e2ac1f88bd7fd7d0d1b9a712ce0137b1
Michael Camilleri <mike@inqk.net>
2022-11-29 02:43:52 +0900

    Fix parsing of the Atlantic

--- src/input/the_atlantic.rb
+++ src/input/the_atlantic.rb
@@ -6,42 +6,29 @@ class TheAtlantic
   def feed
     url = "https://www.theatlantic.com/magazine/"
 
-    date = { path: "h1.c-section-header__title",
-             processor: lambda { |date, rule| normalise_date(date) },
+    date = { path: "main h1",
              type: "datetime",
              prepend: "1 " }
 
-    link = { path: selector("> a"),
-             content: { attribute: "href" },
-             prepend: "https://theatlantic.com" }
+    link = { path: "h3 > a",
+             content: { attribute: "href" } }
 
     info = { id: { literal: url },
              updated: date,
              title: { literal: "The Atlantic" },
              author: { literal: "The Atlantic Monthly Group" } }
 
+    entries = "div[class*=Item_info]"
+
     entry = { id: link,
               updated: date.merge!({ repeat: true }),
-              title: selector("h2.hed"),
-              author: selector("li.byline"),
+              title: "h3",
+              author: "a[class*=_authorLink]",
               link: link,
-              summary: selector("p.dek") }
+              summary: "p" }
 
-    rules = { info: info, entry: entry }
+    rules = { info: info, entries: entries, entry: entry }
 
     Feedstock.feed url, rules
-  end
-
-  private def normalise_date(date)
-    return date unless date.include?("/")
-
-    date.gsub(/\/\w+/, "")
-  end
-
-  private def selector(partial)
-    ["ul[id*=section-Cover] > li.article #{partial},",
-     "ul#section-Features > li.article #{partial},",
-     "ul#section-Dispatches > li.article #{partial},",
-     "ul[id*=section-Part] > li.article #{partial}"].join(" ")
   end
 end
